b53238faa8917186ee73e85322ebe7bb64a4881b
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-03 10:41:17 -0700

    spaces output

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if len(args) == 0:
 else:
     print("{} arguments:".format(len(args)))
 for arg in range(len(args)):
-    print("{} : {}".format(arg + 1, args[arg]))
+    print("{}: {}".format(arg + 1, args[arg]))
