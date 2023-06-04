d5a99dc389528e058f4031fc9b8ca4aa53ae88ca
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-04 16:54:43 -0700

    not executed when imported

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
 from sys import argv
+if __name__ == "__main__":
 
-args = argv[1:]
+    args = argv[1:]
 
-if len(args) == 0:
+    if len(args) == 0:
 
-    print("{} arguments.".format(len(args)))
-elif len(args) == 1:
-    print("{} argument:".format(len(args)))
-else:
-    print("{} arguments:".format(len(args)))
-for arg in range(len(args)):
-    print("{}: {}".format(arg + 1, args[arg]))
+        print("{} arguments.".format(len(args)))
+    elif len(args) == 1:
+        print("{} argument:".format(len(args)))
+    else:
+        print("{} arguments:".format(len(args)))
+    for arg in range(len(args)):
+        print("{}: {}".format(arg + 1, args[arg]))
