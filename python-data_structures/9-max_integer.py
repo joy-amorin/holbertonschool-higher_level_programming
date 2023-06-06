b229e431fb94442561d3d88332e612f5bc97028e
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-06 13:47:50 -0700

    9-max_integer.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if my_list == []:
+        return None
+
+    max_n = my_list[0]
+    for num in my_list:
+        if num > max_n:
+            max_n = num
+            print(max_n)
