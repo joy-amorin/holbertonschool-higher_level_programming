cc515246882f66c5798f2f0333f59107d6ec8f5a
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-06 11:22:46 -0700

    syntax

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d} ".format(element), end='')
-            if element != row[-1]:
-                print(" ", end="")
+            print("{:d}".format(element), end=" "if element != row[-1] else "")
         print()
