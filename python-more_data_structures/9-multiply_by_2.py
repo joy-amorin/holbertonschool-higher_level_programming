b1125f9baf40490140cf35f8efdbfe414aa2a03e
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-07 13:32:43 -0700

    fix output

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
     for key in new_dic:
-        if new_dic[key] % 2 == 0:
-            return new_dic
+        new_dic[key] *= 2
+    return new_dic
