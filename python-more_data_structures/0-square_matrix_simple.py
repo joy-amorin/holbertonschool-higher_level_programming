68065c6df74130634552cf71bc9b4612a45f8522
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-07 08:16:11 -0700

    style

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    square = list(map(lambda row: list(map(lambda x: x ** 2, row)), new_matrix))
+    new_m = matrix.copy()
+    square = list(map(lambda row: list(map(lambda x: x ** 2, row)), new_m))
 
     return square
