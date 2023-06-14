bb7183d1947e049a6b58e0e581f803e7ec2a8a84
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-14 07:01:16 -0700

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,9 +48,12 @@ class Square:
     def my_print(self):
         """prints in stdout the square with the character"""
 
-        if self.__size != 0:
+        if self.__size == 0:
+            print()
+        else:
             for _ in range(self.__position[1]):
                 print()
+
             for _ in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
