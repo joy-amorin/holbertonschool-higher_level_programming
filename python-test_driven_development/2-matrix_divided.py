fb72b9a4d1a9545cc5e6fd284435c73cefc24a56
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-15 08:18:49 -0700

    style

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-""" divides all elements of a matrix"""
+
+"""
+divides all elements of a matrix
+
+
+"""
+
 
 def matrix_divided(matrix, div):
-    """function to divides elements """
+    """
+    function to divides elements
+    """
+
     size_r = len(matrix[0])
     for row in matrix:
         if len(row) != size_r:
@@ -12,7 +21,8 @@ def matrix_divided(matrix, div):
         new_row = []
         for i in row:
             if type(i) not in (int, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats")
             if type(div) not in (int, float):
                 raise TypeError("div must be a number")
             if div == 0:
