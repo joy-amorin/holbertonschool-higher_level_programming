b7316d6277e00e79b87691efc8838eb10bcc3436
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-15 10:52:27 -0700

    4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that prints a square with the character #
+
+"""
+
+
+def print_square(size):
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if float(size) < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print("#" * (size))
