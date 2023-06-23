a9b39ae0af11756c98b45ce3e0610aa6867f4eed
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-23 10:55:21 -0700

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+define a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
