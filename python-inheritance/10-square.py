bda8a99bcb543074b5e5762a0c00befbd4d0d72d
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-23 14:08:53 -0700

    10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""define a Square class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """return area"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """return str"""
+
+        return f"[Rectangle] {self.__size}/{self.__size}"
