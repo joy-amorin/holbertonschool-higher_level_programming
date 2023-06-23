1a913740bf464d915d6076d01d40255d3bb46c4b
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-23 09:23:36 -0700

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+check if a onbject is as instance of the class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function that returns True if the object is exactly
+    an instance of the specified class
+    """
+
+    return isinstance(obj, a_class)
