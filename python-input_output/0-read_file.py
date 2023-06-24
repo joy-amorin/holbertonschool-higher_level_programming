c87e2cd3061bdae9ab29576fa5bb96ceb33727f3
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-24 10:07:08 -0700

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" define a function to read a file
+"""
+
+
+def read_file(filename=""):
+    """function to read a file"""
+
+    with open(filename, 'r') as f:
+        read_data = f.read()
+        print(read_data)
