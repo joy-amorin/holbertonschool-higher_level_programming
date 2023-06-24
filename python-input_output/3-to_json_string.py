e6b7fe08471ce7c2a3005ce781861506443524ca
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-24 12:22:36 -0700

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+to JSON string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
