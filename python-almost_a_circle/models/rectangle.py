6993fdb8bb56b20d937915172d35a10b9c9dfd07
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-26 10:33:51 -0700

    comments

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""define class Rectangle"""
+"""
+define class Rectangle
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
     """class Rectangle inherits from Base """
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ class constructor"""
+        """class constructor"""
 
         self.__width = width
         self.__height = height
