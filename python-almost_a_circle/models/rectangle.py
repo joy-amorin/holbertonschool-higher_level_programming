1cb83d659a6c65d545bb170d2080cf6c7a332723
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-26 11:27:05 -0700

    getters and setters

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,9 +11,48 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
 
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
 
-        super().__init__(id)
+    @property
+    def width(self):
+        """return width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """set width"""
+        self.__width = width
+
+    @property
+    def height(self):
+        """return height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """set height"""
+        self.__height = height
+
+    @property
+    def x(self):
+        """return x"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """set x"""
+        self.__x = x
+
+    @property
+    def y(self):
+        """return y"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """set y"""
+        self.__x = y
