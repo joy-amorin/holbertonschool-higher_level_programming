f6bfbad9b6434882e005675c5183235f73fc42fd
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-26 11:45:27 -0700

    try fix check 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -23,9 +23,9 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """set width"""
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -33,9 +33,9 @@ class Rectangle(Base):
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """set height"""
-        self.__height = height
+        self.__height = value
 
     @property
     def x(self):
@@ -43,9 +43,9 @@ class Rectangle(Base):
         return self.__x
 
     @x.setter
-    def x(self, x):
+    def x(self, value):
         """set x"""
-        self.__x = x
+        self.__x = value
 
     @property
     def y(self):
@@ -53,6 +53,6 @@ class Rectangle(Base):
         return self.__y
 
     @y.setter
-    def y(self, y):
+    def y(self, value):
         """set y"""
-        self.__x = y
+        self.__x = value
