362a0309ace72b5739962717f0b16814f1bce74d
Joy-Amorin <amorinkaren2021@gmail.com>
2023-06-26 10:21:08 -0700

    rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin
+"""define class Rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ class constructor"""
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+        super().__init__(id)
+
+        
