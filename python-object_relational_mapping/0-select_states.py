59b191c770362f38e049a17164fc7e08e6f025a4
Joy-Amorin <amorinkaren2021@gmail.com>
2023-07-31 13:44:17 -0700

    0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""ists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+
+def select_stetes():
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         password=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    select_stetes()
