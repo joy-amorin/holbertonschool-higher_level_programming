3d2833b67d4a9ac5fe28e89f70f554c0b987c731
Joy-Amorin <amorinkaren2021@gmail.com>
2023-07-31 16:06:38 -0700

    delete variable

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,19 +3,13 @@
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
 
-def select_states():
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          password=argv[2], db=argv[3])
-    searched = "%" + argv[4] + "%"
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%'".format(searched))
+    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%'".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-
         print(row)
-
-
-if __name__ == "__main__":
-    select_states()
